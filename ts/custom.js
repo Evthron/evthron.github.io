4511e454b1da7a73e5b1e041c4fba23d2bfa3535
Evthron <116652843+Evthron@users.noreply.github.com>
2025-01-08 12:11:41 +0800

    update tweet sqlite-anki

--- ts/custom.js
+++ ts/custom.js
@@ -1,1 +1,95 @@
-(()=>{o("all");var f=document.getElementById("vocab-button-container"),c=f.getElementsByClassName("vocab-button");c[0].className=c[0].className+" active";function o(e){let n,t;for(n=document.getElementsByClassName("vocab-item"),e=="all"&&(e=""),t=0;t<n.length;t++)p(n[t],"show"),n[t].className.indexOf(e)>-1&&d(n[t],"show")}function d(e,n){let t,a,l;for(a=e.className.split(" "),l=n.split(" "),t=0;t<l.length;t++)a.indexOf(l[t])==-1&&(e.className+=" "+l[t])}function p(e,n){let t,a,l;for(a=e.className.split(" "),l=n.split(" "),t=0;t<l.length;t++)for(;a.indexOf(l[t])>-1;)a.splice(a.indexOf(l[t]),1);e.className=a.join(" ")}for(let e=0;e<c.length;e++)c[e].addEventListener("click",function(){let n=document.getElementsByClassName("active");n[0].className=n[0].className.replace(" active",""),o(this.className.replace("vocab-button ","").replace(" active","")),this.className+=" active"});function v(){var e=new Date,n=e.toLocaleTimeString();document.querySelector(".clock").innerHTML=n}setInterval(v,1e3);var r=document.getElementsByTagName("h3").length,g=document.getElementById("component-count");g.innerHTML=r+"/"+m(r);var N=document.querySelector(".progress-bar-level");N.innerHTML="Lv"+i(r);var h=document.querySelector(".progress-bar-experience"),b=(r-s(r))/(m(r)-s(r))*100;h.style.width=b+"%";function m(e){return u(i(e)+1)}function s(e){return u(i(e))}function i(e){return Math.floor(Math.sqrt(e/6+.25)+.5)}function u(e){return 6*e*(e-1)}})();
+(() => {
+  // ns-hugo-imp:/home/Jacky/Blog/blog/evthronblog/themes/stack/assets/ts/components/vocab.ts
+  var btnContainer = document.getElementById("vocab-button-container");
+  var btns = btnContainer.getElementsByClassName("vocab-button");
+  var selectedTags = [];
+  function filterSelection(selectedTags2) {
+    let x, i;
+    x = document.getElementsByClassName("vocab-item-container");
+    for (i = 0; i < x.length; i++) {
+      w3RemoveClass(x[i], "show");
+      let show = true;
+      for (let selectedTag of selectedTags2) {
+        if (!x[i].getAttribute("tags").split(" ").includes(selectedTag)) {
+          show = false;
+        }
+      }
+      if (selectedTags2.length == 0) {
+        show = false;
+      }
+      if (show == true) {
+        w3AddClass(x[i], "show");
+      }
+      if (selectedTags2.includes("all")) {
+        w3AddClass(x[i], "show");
+      }
+    }
+  }
+  function w3AddClass(element, name) {
+    let i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) == -1) {
+        element.className += " " + arr2[i];
+      }
+    }
+  }
+  function w3RemoveClass(element, name) {
+    let i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);
+      }
+    }
+    element.className = arr1.join(" ");
+  }
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+      let filter = this.getAttribute("tag");
+      if (this.className.includes("active")) {
+        selectedTags.splice(selectedTags.indexOf(filter), 1);
+        this.className = this.className.replace(" active", "");
+      } else {
+        selectedTags[selectedTags.length] = filter;
+        this.className += " active";
+      }
+      console.log(selectedTags);
+      filterSelection(selectedTags);
+    });
+  }
+
+  // ns-hugo-imp:/home/Jacky/Blog/blog/evthronblog/themes/stack/assets/ts/components/clock.ts
+  function displayTime() {
+    var date = /* @__PURE__ */ new Date();
+    var time = date.toLocaleTimeString();
+    if (document.querySelector(".clock")) {
+      document.querySelector(".clock").innerHTML = time;
+    }
+  }
+  setInterval(displayTime, 1e3);
+
+  // ns-hugo-imp:/home/Jacky/Blog/blog/evthronblog/themes/stack/assets/ts/components/progress-bar.ts
+  var element_count = document.getElementsByTagName("h3").length;
+  var experience = document.getElementById("component-count");
+  experience.innerHTML = element_count + "/" + levelCap(element_count);
+  var level = document.querySelector(".progress-bar-level");
+  level.innerHTML = "Lv" + experienceToLevel(element_count);
+  var bar = document.querySelector(".progress-bar-experience");
+  var percentage = (element_count - levelBottom(element_count)) / (levelCap(element_count) - levelBottom(element_count)) * 100;
+  bar.style.width = percentage + "%";
+  function levelCap(experience2) {
+    return levelToExperience(experienceToLevel(experience2) + 1);
+  }
+  function levelBottom(experience2) {
+    return levelToExperience(experienceToLevel(experience2));
+  }
+  function experienceToLevel(value) {
+    return Math.floor(Math.sqrt(value / 6 + 0.25) + 0.5);
+  }
+  function levelToExperience(level2) {
+    return 6 * level2 * (level2 - 1);
+  }
+})();
